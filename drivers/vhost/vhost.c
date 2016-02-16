4438d9bfc28718b81828b7d0df2ada820479c01a
Greg Kurz <gkurz@linux.vnet.ibm.com>
2016-02-16 15:59:44 +0100

    UPSTREAM: vhost: rename vhost_init_used()
    Looking at how callers use this, maybe we should just rename init_used
    to vhost_vq_init_access. The _used suffix was a hint that we
    access the vq used ring. But maybe what callers care about is
    that it must be called after access_ok.
    
    Also, this function manipulates the vq->is_le field which isn't related
    to the vq used ring.
    
    This patch simply renames vhost_init_used() to vhost_vq_init_access() as
    suggested by Michael.
    
    No behaviour change.
    
    Signed-off-by: Greg Kurz <gkurz@linux.vnet.ibm.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    (cherry picked from commit 80f7d0301e7913f704d3505722f806717c61dff5)
    
    BUG=chromium:708265
    TEST='make allmodconfig && make -j64' succeeds
    
    Change-Id: I78412b65345d6c2c2ebf05d2c9973e960bdc38f1
    Signed-off-by: Chirantan Ekbote <chirantan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/486201
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- drivers/vhost/vhost.c
+++ drivers/vhost/vhost.c
@@ -1153,7 +1153,7 @@ static int vhost_update_avail_event(struct vhost_virtqueue *vq, u16 avail_event)
 	return 0;
 }
 
-int vhost_init_used(struct vhost_virtqueue *vq)
+int vhost_vq_init_access(struct vhost_virtqueue *vq)
 {
 	__virtio16 last_used_idx;
 	int r;
@@ -1176,7 +1176,7 @@ int vhost_init_used(struct vhost_virtqueue *vq)
 	vq->last_used_idx = vhost16_to_cpu(vq, last_used_idx);
 	return 0;
 }
-EXPORT_SYMBOL_GPL(vhost_init_used);
+EXPORT_SYMBOL_GPL(vhost_vq_init_access);
 
 static int translate_desc(struct vhost_virtqueue *vq, u64 addr, u32 len,
 			  struct iovec iov[], int iov_size)
